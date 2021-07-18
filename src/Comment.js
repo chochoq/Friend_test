bfa5ff7e385d45b9b85fd36e1fb5524d7439c642
chochoq <aubepe@gmail.com>
2021-07-18 17:10:01 +0900

    (add) comment,Rank.js view

--- src/Comment.js
+++ src/Comment.js
@@ -4,7 +4,40 @@ import styled from 'styled-components';
 const Comment = (props) => {
 
     return (
-        <div>comment</div>
+        <div>
+            <img src="https://file.mk.co.kr/meet/neds/2021/04/image_readtop_2021_374021_16187973224615548.jpg" alt='junhoImg' style={{ width: "80%", margin: "16px" }} />
+            <h1 style={{
+                fontSize: '1.5em',
+                margin: "0px",
+                lineHeight: "1.4",
+            }}>
+                <span 
+                style={{
+                    background: '#fef5d4',
+                    padding: "5px 10px",
+                    borderRadius: '30px'
+                }}>name</span> 에게 한마디
+            </h1>
+            
+            <input type="text" placeholder="이름을 적어주세요"
+                style={{
+                    padding: '10px',
+                    margin: '24px 0px',
+                    border: '1px solid #dadafc',
+                    borderRadius: '30px',
+                    width: "60%"
+                }}>
+            </input>
+
+            <button
+                style={{
+                    padding: "8px 24px",
+                    background: "#dadafc",
+                    borderRadius: '30px',
+                    border: '#dadafc'
+                }}>
+                한마디하고 랭킹보러 갑시다</button>
+        </div>
     )
 }
 
--- src/Rank.js
+++ src/Rank.js
@@ -4,10 +4,39 @@ import styled from 'styled-components';
 const Rank = (props) => {
 
     return (
-        <div>랭크자리</div>
+        <div>
+            <h3>n명의 사람들 중 당신의 점수는?</h3>
+            <hr></hr>
+            <Box>
+                <Text>
+                    <h1>n등</h1>
+                </Text>
+                <p>이름</p> <p>메세지</p>
+            </Box>
+            <Button>다시하기</Button>
+        </div>
     );
 
 }
+
+const Box = styled.div`
+    border: 1px solid #bcbcbc;
+    height: 15vh;
+`
+
+const Text = styled.div`
+    position: absolute;
+    left: 20%;
+`
+
+const Button = styled.button`
+    background-color: rgba(171, 106, 184, 0.411);
+    border-radius: 10px;
+    padding: 10px;
+    border: 0px solid rgba(171, 106, 184, 0.411);
+    margin: 5px;
+    width: 50vw;
+`
 
 export default Rank;
 
