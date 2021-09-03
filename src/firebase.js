bad645ce065ed4dece0a5fd87a5e13c845e823d4
chochoq <aubepe@gmail.com>
2021-09-03 21:47:54 +0900

    (edit) 코드정리

--- src/firebase.js
+++ src/firebase.js
@@ -4,8 +4,13 @@ import "firebase/firestore";
 
 const firebaseConfig = {
     // firebase 설정과 관련된 개인 정보
-    
-
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENTID
 };
 
 // firebaseConfig 정보로 firebase 시작
